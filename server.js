8ac573e371b22e2a006ff3493a0c91266faeb32b
harshit0501 <harshitgoel0501@gmail.com>
2020-10-09 12:18:31 +0530

    Controller methods added

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 //load env variables
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+
+app.use(logger);
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
