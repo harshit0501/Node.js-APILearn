fd214707420c0737f9a67e2e568a1744f26c5cc7
harshit0501 <harshitgoel0501@gmail.com>
2020-10-13 15:39:54 +0530

    user model, authorization, cookie, protect middleware, roles

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const colors = require('colors');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 
 //load env variables
 dotenv.config({ path: "./config/config.env" });
@@ -16,11 +17,15 @@ connectDB();
 //Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 const app = express();
 
 //Body parser
 app.use(express.json());
+
+// Cookie Parser
+app.use(cookieParser());
 
 // Dev logging middleware
 if(process.env.NODE_ENV === 'development') {
@@ -36,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
